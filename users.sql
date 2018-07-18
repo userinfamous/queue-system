86a0bcf321e40615c9ff72a43168aae66991dd33
user.infamous <31716591+vuottek-un@users.noreply.github.com>
2018-07-18 18:22:49 +0700

    update database

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 16, 2018 at 02:05 PM
+-- Generation Time: Jul 18, 2018 at 11:20 AM
 -- Server version: 5.6.34-log
 -- PHP Version: 7.2.1
 
@@ -46,6 +46,24 @@ CREATE TABLE `admins` (
 INSERT INTO `admins` (`ID`, `Name`, `Email`, `Username`, `Password`, `Department`, `People_Served`, `Register_Date`) VALUES
 (17, 'Vuottek Un', 'ianimate39@gmail.com', 'userinfamous', '$5$rounds=535000$sZr52O7/KOCEHizV$EdVsk6jjr01iW8udeWs17rHgLqb6cdXPQEIkCnrIpt.', 'it_dp', NULL, '2018-07-15 05:12:31');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `queue`
+--
+
+CREATE TABLE `queue` (
+  `Number` int(10) NOT NULL,
+  `Type` varchar(20) NOT NULL,
+  `Request` varchar(100) NOT NULL,
+  `Fullname` varchar(50) NOT NULL,
+  `Contact` int(15) NOT NULL,
+  `Student_id` int(10) NOT NULL,
+  `Status` varchar(50) NOT NULL,
+  `Time_in` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Time_out` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -57,6 +75,12 @@ ALTER TABLE `admins`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `queue`
+--
+ALTER TABLE `queue`
+  ADD KEY `Number` (`Number`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -64,7 +88,12 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;COMMIT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+--
+-- AUTO_INCREMENT for table `queue`
+--
+ALTER TABLE `queue`
+  MODIFY `Number` int(10) NOT NULL AUTO_INCREMENT;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
