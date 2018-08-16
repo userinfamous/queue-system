9d395d4065ee9f198dd7497f3173b1ab582d8441
user.infamous <31716591+vuottek-un@users.noreply.github.com>
2018-08-16 13:21:57 +0700

    updated database

--- database/users.sql
+++ database/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 13, 2018 at 02:20 AM
+-- Generation Time: Aug 16, 2018 at 06:18 AM
 -- Server version: 5.6.34-log
 -- PHP Version: 7.2.1
 
@@ -35,13 +35,6 @@ CREATE TABLE `academic_queue` (
   `Time_diff` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `academic_queue`
---
-
-INSERT INTO `academic_queue` (`Number`, `Time_in`, `Time_out`, `Time_diff`) VALUES
-(10, '2018-08-11 15:12:16', '2018-08-11 12:55:08', '00:00:04');
-
 -- --------------------------------------------------------
 
 --
@@ -60,9 +53,7 @@ CREATE TABLE `accounting_queue` (
 --
 
 INSERT INTO `accounting_queue` (`Number`, `Time_in`, `Time_out`, `Time_diff`) VALUES
-(1, '2018-08-10 12:40:07', '2018-08-10 12:41:14', '00:01:07'),
-(10, '2018-08-11 12:55:08', '0000-00-00 00:00:00', '00:00:00'),
-(19, '2018-08-11 15:55:48', '2018-08-11 15:53:02', '00:00:43');
+(1, '2018-08-16 05:34:17', '0000-00-00 00:00:00', '00:00:00');
 
 -- --------------------------------------------------------
 
@@ -102,16 +93,6 @@ CREATE TABLE `front_desk_queue` (
   `Time_out` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `Time_diff` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `front_desk_queue`
---
-
-INSERT INTO `front_desk_queue` (`Number`, `Time_in`, `Time_out`, `Time_diff`) VALUES
-(1, '2018-08-10 12:41:14', '2018-08-10 12:41:27', '00:00:13'),
-(3, '2018-08-10 12:58:31', '2018-08-10 12:59:22', '00:00:00'),
-(4, '2018-08-10 13:05:25', '2018-08-10 14:33:27', '01:28:02'),
-(19, '2018-08-11 15:55:39', '0000-00-00 00:00:00', '00:00:00');
 
 -- --------------------------------------------------------
 
@@ -161,6 +142,7 @@ CREATE TABLE `total_queue` (
   `Student_name` varchar(40) NOT NULL,
   `Student_id` varchar(11) NOT NULL,
   `Status` varchar(30) NOT NULL DEFAULT 'Waiting',
+  `Counter_number` varchar(2) NOT NULL,
   `Total_time` time NOT NULL DEFAULT '00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -168,20 +150,8 @@ CREATE TABLE `total_queue` (
 -- Dumping data for table `total_queue`
 --
 
-INSERT INTO `total_queue` (`Number`, `User_type`, `Request_type`, `Parent_name`, `Contact`, `Student_name`, `Student_id`, `Status`, `Total_time`) VALUES
-(1, 'Parent', 'Academic Enquiry', 'Test1', '', 'Vuottek ', '3484', 'Completed', '00:01:20'),
-(4, 'Parent', 'Bus Service', 'Test3', '', 'Vuottek Un', '3484', 'Completed', '01:28:02'),
-(5, 'Parent', 'Academic Enquiry', 'Vuottek Un', '', 'Vuottek Un', '3484', 'Completed', '00:00:00'),
-(10, 'Student', 'Academic Enquiry', '', '', '', '', 'Completed', '00:00:00'),
-(11, 'Student', 'Transcript/Report Card', '', '', '', '', 'Completed', '00:00:00'),
-(12, 'Parent', 'Transcript/Report Card', 'Test1', '', 'Vuottek Un', '3484', 'Completed', '00:00:00'),
-(13, 'Parent', '', 'Vuottek Un', '', 'Vuottek Un', '3484', 'Waiting', '00:00:00'),
-(14, 'Parent', '', 'Test1', '', 'Vuottek Un', '3484', 'Waiting', '00:00:00'),
-(15, 'Parent', '', 'sada', '', 'asdas', 'asdas', 'Waiting', '00:00:00'),
-(16, 'Parent', 'Academic Paperwork', 'Name', '', 'Vuottek ', '3484', 'Completed', '00:00:00'),
-(17, 'Parent', '', 'Test2', '', 'Vuottek Un', '3484', 'Waiting', '00:00:00'),
-(18, 'Visitor', '', 'James Bond', 'potatoe phone', '', '', 'Waiting', '00:00:00'),
-(19, 'Student', 'Tuition Fee', '', '', 'James Bond', 'potatoe pho', 'In Progress', '00:00:00');
+INSERT INTO `total_queue` (`Number`, `User_type`, `Request_type`, `Parent_name`, `Contact`, `Student_name`, `Student_id`, `Status`, `Counter_number`, `Total_time`) VALUES
+(1, 'Parent', 'Finance Enquiry', 'Test1', '', 'adasd', '', 'In Progress', '1', '00:00:00');
 
 -- --------------------------------------------------------
 
@@ -243,12 +213,12 @@ ALTER TABLE `total_queue`
 -- AUTO_INCREMENT for table `academic_queue`
 --
 ALTER TABLE `academic_queue`
-  MODIFY `Number` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `Number` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `accounting_queue`
 --
 ALTER TABLE `accounting_queue`
-  MODIFY `Number` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `Number` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `admins`
 --
@@ -258,12 +228,12 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `front_desk_queue`
 --
 ALTER TABLE `front_desk_queue`
-  MODIFY `Number` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `Number` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `total_queue`
 --
 ALTER TABLE `total_queue`
-  MODIFY `Number` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;COMMIT;
+  MODIFY `Number` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
